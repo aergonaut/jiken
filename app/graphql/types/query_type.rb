3682244ccd5a9dccc7874f2aeecd9ac981fbbf10
Chris Fung <aergonaut@gmail.com>
2018-10-25 17:04:10 -0700

    adding apollo

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,6 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
     field :services, ServiceType.connection_type, null: false
+
+    def services
+      Service.all
+    end
     field :service, ServiceType, null: true do
       argument :id, ID, required: true
     end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,10 @@ class Service < ApplicationRecord
   has_many :incidents
 
   validates :name, presence: true, length: { maximum: 255 }
+
+  def status
+    return "IMPACTED" if incidents.any? { |incident| incident.status == "OPEN" }
+    return "MONITORING" if incidents.any? { |incident| incident.status == "MONITORING" }
+    "OPERATIONAL"
+  end
 end
