8c0d7f06092279823b72295c731bf127ec311cd7
Chris Fung <aergonaut@gmail.com>
2018-10-26 09:20:07 -0700

    service status ui

--- app/javascript/packs/incidents_app.tsx
+++ app/javascript/packs/incidents_app.tsx
@@ -1,19 +1,55 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import '../incidents/styles/main';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
 
-const Hello = (props: any) => (
-  <div>
-    Hello
-    {props.name}!
-  </div>
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: '/graphql',
+      credentials: 'same-origin',
+    }),
+  ]),
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="container-md px-3">
+      <h2 className="f1-light border-bottom mb-4">Service Status</h2>
+      <div className="Box Box-body mb-3">
+        <div className="d-flex flex-justify-between">
+          <div>Webhooks</div>
+          <div>Operational</div>
+        </div>
+      </div>
+      <div className="Box Box-body Box--danger text-red">
+        <div className="d-flex flex-justify-between">
+          <div>Web UI</div>
+          <div>Impacted</div>
+        </div>
+      </div>
+    </div>
+  </ApolloProvider>
 );
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App />,
     document.body.appendChild(document.createElement('div'))
   );
 });
